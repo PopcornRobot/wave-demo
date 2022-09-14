fa497703c5ab79e586215583e5d72c242f06e013
Brandon Madison <brandmadis@gmail.com>
2022-09-14 10:01:34 -0700

    asgi mod

--- wave_demo/asgi.py
+++ wave_demo/asgi.py
@@ -3,11 +3,11 @@ import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-django.setup()
 
 import app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wave_demo.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
