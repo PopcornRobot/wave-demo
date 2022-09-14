426ad2b07129ee18bf608d563d4c0997874856f2
Brandon Madison <brandmadis@gmail.com>
2022-09-14 10:36:44 -0700

    heroku db

--- wave_demo/settings.py
+++ wave_demo/settings.py
@@ -96,17 +96,26 @@ CHANNEL_LAYERS = {
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DATABASE_NAME"), 
+#         'USER': env("DATABASE_USER"), 
+#         'PASSWORD': env("DATABASE_PASSWORD"),
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DATABASE_NAME"), 
-        'USER': env("DATABASE_USER"), 
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
