ad14d1ce2bca44960857e57b3e9587f159fb2b9d
Brandon Madison <brandmadis@gmail.com>
2022-09-14 09:47:45 -0700

    init websockets

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "app/index.html")
+
+def room (request, room_name):
+    print("----", room_name)
+    username = request.GET.get('username', 'Anonymous')
+    return render(request, 'app/room.html', {'room_name': room_name, 'username': username})
