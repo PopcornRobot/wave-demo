6e2a3ccf60caae83e0b9972c6a501c3d93d1b8b7
Brandon Madison <brandmadis@gmail.com>
2022-09-14 17:29:51 -0700

    separate local and production settings

--- wave_demo/settings.py
+++ wave_demo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+print("---------- main settings")
 import environ
 import os
 from pathlib import Path
