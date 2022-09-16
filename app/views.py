1145130fc76060644e28091b0fda73f4502a4439
Brandon Madison <brandmadis@gmail.com>
2022-09-15 18:21:09 -0700

    test websocket connections

--- app/views.py
+++ app/views.py
@@ -1,29 +1,37 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-from app.models import Room
+from app.models import Room, Player
 
 def index(request):
     return render(request, "app/index.html")
 
 def room1(request, room_name):
-    print("----", room_name)
+    print("---- 1 ", room_name)
     username = request.GET.get('username', 'Anonymous')
+    player = Player.objects.create(username="test")
+    print("--- ", player)
     return render(request, 'app/room_1.html', {'room_name': room_name, 'username': username})
 
 def room2(request, room_name):
     print("----", room_name)
     username = request.GET.get('username', 'Anonymous')
+    player = Player.objects.create(username="test")
+
     return render(request, 'app/room_2.html', {'room_name': room_name, 'username': username})
 
 def room3(request, room_name):
     print("----", room_name)
     username = request.GET.get('username', 'Anonymous')
+    player = Player.objects.create(username="test")
+
     return render(request, 'app/room_3.html', {'room_name': room_name, 'username': username})
 
 def room4(request, room_name):
     print("----", room_name)
     username = request.GET.get('username', 'Anonymous')
+    player = Player.objects.create(username="test")
+
     return render(request, 'app/room_4.html', {'room_name': room_name, 'username': username})
 
 
